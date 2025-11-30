cf0353a21deae6b224d8058af9706a44a6719ab2
Ice713 <icramzilruiz@gmail.com>
2025-11-30 21:50:00 +0800

    Created UI and Course Class.
    Next would be OnlineCourse Class and Event-handler.

---
+++ 9. E-Learning System/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.E_Learning_System
+{
+    internal class Course
+    {
+        public string Title { get; set; }
+        public string Duration { get; set; }
+
+        public Course(string title, string duration) 
+        {
+            Title = title;
+            Duration = duration;
+        }
+
+        public virtual string DisplayDetails()
+        {
+            return $"Course: {Title}\nDuration: {Duration}\n";
+        }
+    }
+}
--- 9. E-Learning System/Form1.Designer.cs
+++ 9. E-Learning System/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxDuration = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPlatform = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "9. E-Learning System ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Course: ";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(197, 99);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(172, 20);
+            this.textBoxTitle.TabIndex = 2;
+            // 
+            // textBoxDuration
+            // 
+            this.textBoxDuration.Location = new System.Drawing.Point(197, 125);
+            this.textBoxDuration.Name = "textBoxDuration";
+            this.textBoxDuration.Size = new System.Drawing.Size(172, 20);
+            this.textBoxDuration.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Duration:";
+            // 
+            // textBoxPlatform
+            // 
+            this.textBoxPlatform.Location = new System.Drawing.Point(197, 151);
+            this.textBoxPlatform.Name = "textBoxPlatform";
+            this.textBoxPlatform.Size = new System.Drawing.Size(172, 20);
+            this.textBoxPlatform.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Platform:";
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisplay.Location = new System.Drawing.Point(119, 190);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(202, 41);
+            this.buttonDisplay.TabIndex = 7;
+            this.buttonDisplay.Text = "Display Details";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(482, 267);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.textBoxPlatform);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDuration);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "9. E-Learning System ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxDuration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPlatform;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonDisplay;
     }
 }
 
